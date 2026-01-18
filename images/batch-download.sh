6a0804eee1510f250ad20680875484ebd39b0c08
Obsidian User <obsidian@local>
2026-01-18 21:33:58 +0800

    auto-commit

---
+++ images/batch-download.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Prompt 3: Coloring page
+curl -L -o "3-coloring-2.jpg" "https://pbs.twimg.com/media/G7NfigIW8AESnWy?format=jpg&name=900x900"
+curl -L -o "3-coloring-3.jpg" "https://pbs.twimg.com/media/G7MEwTWWEAA1DkO?format=jpg&name=medium"
+curl -L -o "3-coloring-4.jpg" "https://pbs.twimg.com/media/G7RxEw8XsAE5Y2q?format=jpg&name=medium"
+
+# Prompt 4: Home office
+curl -L -o "4-home-office-1.jpg" "https://pbs.twimg.com/media/G7MEwTWWEAA1DkO?format=jpg&name=large"
+curl -L -o "4-home-office-2.jpg" "https://pbs.twimg.com/media/G7HqLdcXcAEVoJq?format=jpg&name=large"
+
+# Prompt 5: Cyber profile  
+curl -L -o "5-cyber-1.jpg" "https://pbs.twimg.com/media/G7HqLdcXcAEVoJq?format=jpg&name=large"
+curl -L -o "5-cyber-2.jpg" "https://pbs.twimg.com/media/G7BWvI8X0AApeZB?format=jpg&name=900x900"
+curl -L -o "5-cyber-3.jpg" "https://pbs.twimg.com/media/G7BWvJBXgAAV947?format=jpg&name=900x900"
+curl -L -o "5-cyber-4.jpg" "https://pbs.twimg.com/media/G7BWvIzWIAAr6Tz?format=jpg&name=900x900"
+curl -L -o "5-cyber-5.jpg" "https://pbs.twimg.com/media/G7BW6F-XYAAN1iu?format=jpg&name=900x900"
+curl -L -o "5-cyber-6.jpg" "https://pbs.twimg.com/media/G7BXMwjXUAA5TXM?format=jpg&name=medium"
+
+# Prompt 6: Chibi store
+curl -L -o "6-chibi-1.jpg" "https://pbs.twimg.com/media/G7BWvI8X0AApeZB?format=jpg&name=900x900"
+curl -L -o "6-chibi-2.jpg" "https://pbs.twimg.com/media/G7BWvJBXgAAV947?format=jpg&name=900x900"
+curl -L -o "6-chibi-3.jpg" "https://pbs.twimg.com/media/G7BWvIzWIAAr6Tz?format=jpg&name=900x900"
+curl -L -o "6-chibi-4.jpg" "https://pbs.twimg.com/media/G7BW6F-XYAAN1iu?format=jpg&name=900x900"
+curl -L -o "6-chibi-5.jpg" "https://pbs.twimg.com/media/G7JDW8cbgAEOHHs?format=jpg&name=900x900"
+curl -L -o "6-chibi-6.jpg" "https://pbs.twimg.com/media/G7JDW8NbgAMgLx8?format=jpg&name=900x900"
+curl -L -o "6-chibi-7.jpg" "https://pbs.twimg.com/media/G7JDXBfagAAddrb?format=jpg&name=900x900"
+curl -L -o "6-chibi-8.jpg" "https://pbs.twimg.com/media/G7JDW8NaIAAV3uW?format=jpg&name=900x900"
+curl -L -o "6-chibi-9.jpg" "https://pbs.twimg.com/media/G7qJxLfaEAAljgQ?format=jpg&name=medium"
+
+# Prompt 7: Travel journal
+curl -L -o "7-travel-1.jpg" "https://pbs.twimg.com/media/G69WHFDW4AAv0TK?format=jpg&name=medium"
+curl -L -o "7-travel-2.jpg" "https://pbs.twimg.com/media/G69WJkjWsAApkIu?format=jpg&name=medium"
+curl -L -o "7-travel-3.jpg" "https://pbs.twimg.com/media/G69WJi3WkAAhV31?format=jpg&name=medium"
+curl -L -o "7-travel-4.jpg" "https://pbs.twimg.com/media/G6smYVJX0AAmgko?format=jpg&name=medium"
+curl -L -o "7-travel-5.jpg" "https://pbs.twimg.com/media/G6smZsqWcAETTUC?format=jpg&name=medium"
+
+# Prompt 8: Comic
+curl -L -o "8-comic-1.jpg" "https://pbs.twimg.com/media/G60vNn8WcAE0Gl0?format=jpg&name=medium"
+curl -L -o "8-comic-2.jpg" "https://pbs.twimg.com/media/G603cshXUAATaTW?format=jpg&name=large"
+
+# Prompt 9: Calendar
+curl -L -o "9-calendar-1.jpg" "https://pbs.twimg.com/media/G6s8yReWEAAHNnR?format=jpg&name=medium"
+curl -L -o "9-calendar-2.jpg" "https://pbs.twimg.com/media/G6s8znOaoAAhJxy?format=jpg&name=medium"
+curl -L -o "9-calendar-3.jpg" "https://pbs.twimg.com/media/G6s9DrSWwAIIUen?format=jpg&name=medium"
+curl -L -o "9-calendar-4.jpg" "https://pbs.twimg.com/media/G6s9JACXAAAQnJs?format=jpg&name=medium"
+curl -L -o "9-calendar-5.jpg" "https://pbs.twimg.com/media/G6s9fhEXMAAdpvo?format=jpg&name=medium"
+
+# Prompt 10: Gongbi
+curl -L -o "10-gongbi-1.jpg" "https://pbs.twimg.com/media/G6aa9VgWcAAklY6?format=jpg&name=medium"
+curl -L -o "10-gongbi-2.jpg" "https://pbs.twimg.com/media/G6ZOMb_WEAAVKF6?format=jpg&name=medium"
+curl -L -o "10-gongbi-3.jpg" "https://pbs.twimg.com/media/G6ac3vXXwAAf2oF?format=jpg&name=medium"
+curl -L -o "10-gongbi-4.jpg" "https://pbs.twimg.com/media/G6mz9D7bwAM8tnm?format=jpg&name=small"
+curl -L -o "10-gongbi-5.jpg" "https://pbs.twimg.com/media/G6mz9EqaUAAsgWw?format=jpg&name=small"
+curl -L -o "10-gongbi-6.jpg" "https://pbs.twimg.com/media/G6mz9CeaoAAVYXC?format=jpg&name=small"
+
